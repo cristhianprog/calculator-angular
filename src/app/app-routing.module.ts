a0027c8c982d98cd4c0c3c8649192a6852194752
cristhianprog <cristhianprog@gmail.com>
2021-01-09 02:37:34 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+// Angular
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+// Components
+import { CalculatorComponent } from './calculator/calculator.component';
+
+const routes: Routes = [
+	
+	{
+		path: 'calculator',
+		component: CalculatorComponent
+	}
+
+];
+
+
+@NgModule({
+	imports: [
+		RouterModule.forRoot(routes),
+	],
+	exports: [RouterModule],
+})
+
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
