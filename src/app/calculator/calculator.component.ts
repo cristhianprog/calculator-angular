9cf14208986acc27cc0798e7c6298890a78f681e
cristhianprog <cristhianprog@gmail.com>
2021-01-09 23:33:03 -0300

    criando funçoes

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -8,10 +8,68 @@ import { Component, OnInit } from '@angular/core';
 export class CalculatorComponent implements OnInit {
 
   public isCollapsed = true;
+  public numbDisplay: String = "0";
+  public dataDisplay: String = "0";
+  public dataOperator: String;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  calcBackEnd(firstNumb, secNumb, operator){
+    let first = parseInt(firstNumb);
+    let second = parseInt(secNumb);
+   
+    let result;
+
+    switch (operator) {
+      case '+':
+        result = first + second;
+        break;
+      case '-':
+        result = first - second;
+        break;
+      case 'x':
+        result = first * second;
+        break;
+      case '/':
+        result = first / second;
+        break;
+
+    }
+
+    this.numbDisplay = result.toString(); //trocar quando for no back-end
+
+  }
+
+  clickButtonNumb(number): void {
+    if(this.numbDisplay == "0"){
+      this.numbDisplay = "";
+
+    }
+
+    this.numbDisplay = this.numbDisplay + number;
+  }
+
+  operation(operator): void {
+    this.dataOperator = operator;
+    this.dataDisplay = this.numbDisplay;
+    this.numbDisplay = "0";
+    
+  }
+
+  clearDisplay(): void {
+    this.numbDisplay = "0";
+    this.dataOperator = "";
+  }
+
+  equal(): void {
+    if(this.dataDisplay != ""){
+      this.calcBackEnd(this.dataDisplay, this.numbDisplay, this.dataOperator);
+      this.dataOperator = "";
+    }
+
   }
 
 }
